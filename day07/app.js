71dc1d6fabca605922afb8e34734015e2d027a6f
m. scott veach <mscottveach@gmail.com>
2020-12-10 15:06:50 -0800

    NOT NOW.

--- day07/app.js
+++ day07/app.js
@@ -1,1 +1,40 @@
-document.getElementById("demo").innerHTML = 5 + 6;
+
+
+var cy = cytoscape({
+    container: document.getElementById('cy')
+  });
+
+var cy = cytoscape({
+    container: document.getElementById('cy'),
+    elements: [
+      { data: { id: 'a' } },
+      { data: { id: 'b' } },
+      {
+        data: {
+          id: 'ab',
+          source: 'a',
+          target: 'b'
+        }
+      }]
+  });
+
+// var fs = require('fs');
+// var data = fs.readFileSync('input.txt',"utf8");
+// let data_ros = data.split('\r\n');
+
+// remove_contain = (in_string) => {
+//     return('1 ' + in_string.replace(' contain ',', ').replace(/ bags?\.?/g,''));
+// };
+
+// bag_relationships = data_ros.map(remove_contain);
+// console.log(bag_relationships);
+
+// the_nodes = new Set();
+// bag_relationships.forEach((in_string) => {
+//     bags_in_rule = in_string.split(', ');
+//     bags_in_rule.forEach((a_bag) => {
+//         the_nodes.add(a_bag.slice(2,));
+//     })
+// })
+// console.log(the_nodes);
+// bag_relatioships.forEach()
--- day07/server.js
+++ day07/server.js
@@ -1,15 +1,20 @@
-var http = require('http');
-var fs = require('fs');
 
-const PORT=8080; 
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+let http = require('http');
+let fs = require('fs');
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+ 
+http.createServer(handleRequest).listen(8000);
